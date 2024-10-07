0cab01b3897882aadacba390b737b47dc1b3301c
ntc-reynolds <175027607+ntc-reynolds@users.noreply.github.com>
2024-10-07 16:57:31 -0400

    added extra credit functionality

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-let height = 16;
-let width = 16;
+let pageContainer = document.querySelector(".page-container");
+let sizeButton = document.querySelector("#set-grid-size");
+let clearGridButton = document.querySelector("#clear");
+let sketchContainer = document.querySelector(".sketch-container");
 
-let container = document.querySelector(".container");
-let counter = 1;
-for (let i=0; i<height; i++){
-    let rowflex = document.createElement("div");
-    rowflex.classList.add("rowflex");
-    for (let j=0; j<width;j++){
-        let block = document.createElement("div");
-        block.classList.add("block");
-        block.textContent = `${counter}`;
-        block.addEventListener("mouseover", ()=>{
-            block.style.backgroundColor = "cyan";
-        });
-        block.addEventListener("mouseout", ()=>{
+let size = 20;
+drawGrid(size);
+
+clearGridButton.addEventListener("click",()=>{
+    document.querySelectorAll("div.block").forEach((element)=>{
+        element.style.backgroundColor = "white";
+        element.style.opacity = 0;
+    });
+});
+
+sizeButton.addEventListener("click", ()=>{
+    let userSize;
+    do{
+        userSize = Number(prompt("Enter a number between 8 and 100: "));
+        if (typeof(userSize)=="number"&&userSize<=100&&userSize>=8){
+            removeGrid();
+            drawGrid(userSize);
+            break;
+        }else{
+            continue;
+        };
+    }while(true); 
+});
+function drawGrid(gridSize) {
+    for (let i=0; i<gridSize; i++){
+        let rowflex = document.createElement("div");
+        rowflex.classList.add("rowflex");
+        for (let j=0; j<gridSize;j++){
+            let block = document.createElement("div");
+            let blockOpacity = 0;
+            let blockColour = "white"
+            block.classList.add("block");
             block.style.backgroundColor = "white";
-        });
-        rowflex.appendChild(block);
-        counter++;
+            block.addEventListener("mouseover", function(){
+                if (block.style.backgroundColor == "white"){
+                    randomColor = Math.floor(Math.random()*16777215).toString(16);
+                    block.style.backgroundColor = "#" + randomColor;
+                    block.style.opacity = 0.1;
+                }else if(blockOpacity<1.0){
+                    blockOpacity = Math.min(blockOpacity+0.1, 1.0);
+                    block.style.opacity = blockOpacity;
+                }
+            });
+            rowflex.appendChild(block);
+        }
+        sketchContainer.appendChild(rowflex);
     }
-    container.appendChild(rowflex);
+}
+function removeGrid(){
+    while(sketchContainer.firstChild){
+        sketchContainer.removeChild(sketchContainer.firstChild);
+    }
 }
